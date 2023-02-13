367b3554b2eabd46f483ca32eb57333eb8062397
dharanim18 <86888618+dharanim18@users.noreply.github.com>
2023-02-13 22:30:19 +0530

    Update singleinheritance.java

--- src/scuba2/singleinheritance.java
+++ src/scuba2/singleinheritance.java
@@ -5,7 +5,7 @@ public class singleinheritance {
 		 void call(){System.out.println("calling...");}  
 			}  
 			class inherit extends singleinheritance{  
-			void call1(){System.out.println("calling1...");}  
+			void call1(){System.out.println("calling111...");}  
 			}  
 			class TestInheritance{  
 			public static void main(String args[]){  
