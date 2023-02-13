32a49effcd11f79803af615fda7f9ccea206ca95
dharanim18 <86888618+dharanim18@users.noreply.github.com>
2023-02-13 22:13:07 +0530

    Update Arraylist.java

--- src/arraylist/Arraylist.java
+++ src/arraylist/Arraylist.java
@@ -6,7 +6,7 @@ public class Arraylist {
 		    {
 		        
 		        List<Integer> numbers = Arrays.asList(1, 2, 3,
-		                                       4, 5, 6, 7, 8);
+		                                       4, 5, 6, 7, 8,9);
 		        for (int i = 0; i < numbers.size(); i++)  
 		            System.out.print(numbers.get(i) + " ");       
 		    }
